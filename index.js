2c25e3267f38137fe1655f79225f5ad9c134b4f3
Graeme Foster <graemef@gmail.com>
2013-01-20 17:37:44 +0000

    Optionally supply auth info for each call.

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var request = require('request');
 
 var Sprintly = function(email, apiKey) {
     this.apiUri = 'https://sprint.ly/api';
-    this.auth = function(options) {
+    this.auth = function(options, auth) {
       options.headers = options.headers || {};
-      options.headers.Authorization = 'Basic ' + new Buffer(email + ':' + apiKey).toString('base64');
+      auth = auth || {
+        email: email,
+        apiKey: apiKey
+      };
+      options.headers.Authorization = 'Basic ' + new Buffer(auth.email + ':' + auth.apiKey).toString('base64');
     };
   };
 
@@ -20,26 +24,35 @@ Sprintly.prototype.getPeople = function getPeople(productId, callback) {
   });
 };
 
-Sprintly.prototype.addItem = function addItem(productId, item, callback) {
+Sprintly.prototype.addItem = function addItem(auth, productId, item, callback) {
   var options = {
     method: 'POST',
     uri: this.apiUri + '/products/' + productId + '/items.json',
     form: item
   };
-  this.auth(options);
+  this.auth(options, auth);
 
   request(options, function(error, response, body) {
-    callback(error, body);
+    error = error || response.code >= 400 ? body : null;
+    if (error) {
+      console.error(error);
+      return callback(error);
+    }
+
+    item = JSON.parse(body);
+    callback(error, item);
   });
 };
 
-Sprintly.prototype.addCommentToItem = function addCommentToItem(productId, item_number, comment, callback) {
+Sprintly.prototype.addCommentToItem = function addCommentToItem(auth, productId, item_number, comment, callback) {
   var options = {
     method: 'POST',
     uri: this.apiUri + '/products/' + productId + '/items/' + item_number + '/comments.json',
-    form: comment
+    form: {
+      body: comment
+    }
   };
-  this.auth(options);
+  this.auth(options, auth);
 
   request(options, function(error, response, body) {
     callback(error, body);
